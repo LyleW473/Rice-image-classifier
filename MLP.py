bb420f53c8ed4a4514b7f7805a98a5717a64044f
Lyle Wong <lylereturns@gmail.com>
2023-06-18 22:36:18 +0100

    Adapted "generate_batch" function: - Allows for test/train splits.

--- MLP.py
+++ MLP.py
@@ -5,17 +5,27 @@ import torch
 torch.manual_seed(2000)
 batch_size = 32
 image_folders = os.listdir("Dataset/Images")
-print(image_folders)
 
+# Splits and distributions for generating batches
 num_types = len(image_folders)
-uniform_t_distribution = torch.ones(num_types, dtype = torch.float64) / num_types # 5 types of rice
-uniform_i_distribution = torch.ones(15000, dtype = torch.float64) / 15000 # 15000 images for each type of rice
+uniform_types_distribution = torch.ones(num_types, dtype = torch.float64) / num_types # 5 types of rice
 
-print(uniform_i_distribution.dtype)
-print(uniform_t_distribution.dtype)
-print(uniform_i_distribution)
-print(uniform_t_distribution)
+# 5 types of rice, 15000 images for each = 75000 total
+# 75000 * 0.2 = 15000 images in total for test/validation split
+# 15000 / 5 = 3000 images of each type for test/validation split
+test_split_multiplier = 0.2
+num_test_imgs = int((75000 * test_split_multiplier) / 5)
+num_train_imgs = int(15000 - num_test_imgs)
 
+print(num_test_imgs, num_train_imgs)
+
+uniform_train_images_distribution = torch.ones(num_train_imgs, dtype = torch.float64) / num_train_imgs # 15000 images for each type of rice
+uniform_test_images_distribution = torch.ones(num_test_imgs, dtype = torch.float64) / num_test_imgs
+
+print(uniform_train_images_distribution.dtype)
+print(uniform_types_distribution.dtype)
+print(uniform_types_distribution)
+print(uniform_train_images_distribution)
 
 def img_to_matrix(image_indexes, r_type_indexes):
 
@@ -24,8 +34,8 @@ def img_to_matrix(image_indexes, r_type_indexes):
     # Find all rice types in the selected batch
     rice_types = [image_folders[r_type_i] for r_type_i in r_type_indexes]
 
-    print(rice_types)
-    print(image_indexes)
+    # print(rice_types)
+    # print(image_indexes)
     
     matrices = []
 
@@ -51,20 +61,30 @@ def img_to_matrix(image_indexes, r_type_indexes):
     
     return matrices
     
-def generate_batch(batch_size):
+def generate_batch(batch_size, split):
     
     # Generate indexes for rice types
-    rice_type_indexes = torch.multinomial(input = uniform_t_distribution, num_samples = batch_size, replacement = True)
+    rice_type_indexes = torch.multinomial(input = uniform_types_distribution, num_samples = batch_size, replacement = True)
 
     # Convert the index into a one-hot vector for the entire batch
     rice_types = torch.zeros(batch_size, 5)
     rice_types[torch.arange(batch_size), rice_type_indexes - 1] += 1 # 2nd option = index 1 (Possible indexes = 0, 1, 2, 3, 4 ... num_types)
-
+    
     # -------------------------------
 
-    # Generate indexes for rice images
-    rice_image_indexes = torch.multinomial(input = uniform_i_distribution, num_samples = batch_size, replacement = True)
-
+    # Test split images
+    if split == "Test":
+        # Note: uniform_test_images_distribution will have a smaller range of index values 
+        # - (e.g. if num_test_imgs = 15000, as there are 5 types, the last 15000 / 3000 images are for the test/validation split)
+        # - i.e. Only images at index 12000 -> 15000 will be used for the test, whereas the 0 -> 12000 will be used for the train split
+        rice_image_indexes = torch.multinomial(input = uniform_test_images_distribution, num_samples = batch_size, replacement = True)
+        rice_image_indexes += (15000 - num_test_imgs)
+    
+    # Train split images
+    else:
+        # Generate indexes for rice images
+        rice_image_indexes = torch.multinomial(input = uniform_train_images_distribution, num_samples = batch_size, replacement = True)
+    
     # Convert indexes to matrices
     rice_image_matrices = img_to_matrix(image_indexes = rice_image_indexes, r_type_indexes = rice_type_indexes)
 
@@ -73,6 +93,9 @@ def generate_batch(batch_size):
     return rice_image_matrices, rice_types
         
 
-Xb, Yb = generate_batch(batch_size = batch_size)
+# Shape = (batch_size, number of pixels in each image, 1) (batch_size of 62500 pixels which contain a greyscale value)
+Xtr, Ytr = generate_batch(batch_size = batch_size, split = "Train")
+Xte, Yte = generate_batch(batch_size = batch_size, split = "Test")
 
-print(Xb.shape, Yb.shape)
+print(Xtr.shape, Ytr.shape)
+print(Xte.shape, Yte.shape)
